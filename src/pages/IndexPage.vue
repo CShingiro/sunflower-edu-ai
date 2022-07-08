d49dea5db6d1d11cd7d286996200d6c3e79662f7
CShingiro <nemyent@gmail.com>
2022-07-08 19:05:29 -0400

    Patch to responsiveness on home page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
 
 <template>
   <q-page class="row items-center justify-evenly q-pa-md q-gutter-md">
-    <div class="col">
+    <div class="col gt-md">
       <transition-group
         appear
         enter-active-class="animated fadeInLeft slower"
@@ -18,21 +18,58 @@
           key="slogan"
           class="text-center cairo text-italic text-bold text-green-7"
         >
-          A Sunflower for the CountrySide
+          A Sunflower for the Countrysite
         </h3>
       </transition-group>
     </div>
-    <div class="col">
+    <div class="col-12 lt-lg">
+      <transition-group
+        appear
+        enter-active-class="animated fadeInLeft slower"
+        leave-active-class="animated fadeOutLeft"
+      >
+        <h2
+          key="project-title"
+          class="gt-xs text-center cairo text-italic text-bold text-green-10"
+        >
+          SunflowerEdu AI
+        </h2>
+        <h3
+          key="project-title"
+          class="lt-sm text-center cairo text-italic text-bold text-green-10"
+        >
+          SunflowerEdu AI
+        </h3>
+        <h3
+          key="slogan"
+          class="gt-xs text-center cairo text-italic text-bold text-green-7"
+        >
+          A Sunflower for the CountrySide
+        </h3>
+        <h5
+          key="slogan"
+          class="lt-sm text-center cairo text-italic text-bold text-green-7"
+        >
+          A Sunflower for the CountrySide
+        </h5>
+      </transition-group>
+    </div>
+    <div class="col gt-xs">
       <transition
         appear
         enter-active-class="animated bounceInDown slower"
         leave-active-class="animated bounceOutUp"
       >
-        <q-img
-          class="draw-in"
-          fit="fill"
-          src="/images/SunflowerEduAI.svg"
-        ></q-img>
+        <q-img fit="fill" src="/images/SunflowerEduAI.svg"></q-img>
+      </transition>
+    </div>
+    <div class="col-12 lt-sm">
+      <transition
+        appear
+        enter-active-class="animated fadeInRight slower"
+        leave-active-class="animated fadeOutRight"
+      >
+        <q-img fit="fill" src="/images/SunflowerEduAI.svg"></q-img>
       </transition>
     </div>
     <div class="col">
