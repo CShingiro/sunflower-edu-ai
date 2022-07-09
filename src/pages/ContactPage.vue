cd6e478a07746fd0334e53b0f7fd66eb4254b9f1
CShingiro <nemyent@gmail.com>
2022-07-09 04:52:13 -0400

    Contact Page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page class="row items-center justify-evenly">
+    <h1 class="gt-sm cairo text-center text-italic text-bold text-green-10">
+      Contact
+    </h1>
+    <h2 class="sm cairo text-center text-italic text-bold text-green-10">
+      Contact
+    </h2>
+    <h3 class="lt-sm cairo text-center text-italic text-bold text-green-10">
+      Contact
+    </h3>
+    <h3 class="gt-md nunito-italic text-center">
+      Email the
+      <a href="mailto:info@sunflowereduai.com">SunflowerEdu AI</a> team for more
+      information on this exciting project!
+    </h3>
+    <h4 class="md nunito-italic text-center">
+      Email the
+      <a href="mailto:info@sunflowereduai.com">SunflowerEdu AI</a> team for more
+      information on this exciting project!
+    </h4>
+    <h5 class="lt-md nunito-italic text-center">
+      Email the
+      <a href="mailto:info@sunflowereduai.com">SunflowerEdu AI</a> team for more
+      information on this exciting project!
+    </h5>
+  </q-page>
 </template>
 
 <style scoped></style>
